b4040979873d9fcfbb64df27e4b8beb3412e14de
NatasjaLoevschall <natatasja.loevschall@gmail.com>
2025-10-06 10:24:44 +0200

    nye ændringer
    forside flyttet over i index

---
+++ script.js
@@ -0,0 +1,16 @@
+// tager fat i overskriften
+const overskrift = document.getElementById("stjernensliv");
+// tager fat i knappen
+const knap = document.querySelector("#forside a");
+
+window.onload = function() {
+  // Fade ind på overskriften
+  setTimeout(() => {
+    overskrift.style.opacity = 1;
+  }, 500); // 0,5 sekund
+
+  // Fade ind på knappen lidt senere
+  setTimeout(() => {
+    knap.style.opacity = 1;
+  }, 1500); // 1,5 sekund
+};
