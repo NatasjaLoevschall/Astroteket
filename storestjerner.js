79f14a41d6d4dac39382398a6bbd3cd040b4558b
Paula <paulaengel71@gmail.com>
2025-10-06 12:01:25 +0200

    tilføjet lukke (kryds) knap

--- storestjerner.js
+++ storestjerner.js
@@ -1,22 +1,27 @@
-// tager fat i knappen
-const stjernehobknap = document.querySelector("#stjernehobBtn");
+    // tager fat i knappen
+    const stjernehobknap = document.querySelector("#stjernehobBtn");
 
-window.onload = function() {
-    // Fade ind på knap
-    setTimeout(() => { 
-      stjernehobknap.style.opacity = 1;
-    }, 3500); // 0,5 sekund
-}
+    window.onload = function() {
+        // Fade ind på knap
+        setTimeout(() => { 
+        stjernehobknap.style.opacity = 1;
+        }, 3500); // 0,5 sekund
+    }
 
-document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.getElementById("stjernehobBtn");
-    const videoContainer = document.getElementById("motion1");
-
-    btn.addEventListener("click", function () {
-        if (videoContainer.style.display === "none") {
-            videoContainer.style.display = "block";
-        } else {
-            videoContainer.style.display = "none";
-        }
+    document.addEventListener("DOMContentLoaded", function () {
+        const btn = document.getElementById("stjernehobBtn");
+        const videoContainer = document.getElementById("motion1");
+        const kryds = document.getElementById("kryds");
+    
+        btn.addEventListener("click", function () {
+            videoContainer.style.display = "block"; // viser videoen
+            kryds.style.display = "block"; // viser krydset
+        });
+    
+    //når man klikker på krydset forsvinder både video,stjernehob og krydset
+        kryds.addEventListener("click", function () {
+            videoContainer.style.display = "none"; // skjuler videoen
+            kryds.style.display = "none"; // skjuler krydset
+            stjernehobknap.style.display = "none"; // skjuler knappen
+        });
     });
-});
